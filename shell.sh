c27347c39b5a2ad77b82a6f247795511e0d7281f
Ariful <arif98741@gmail.com>
2020-11-25 19:47:33 +0000

    changes shell for playschoolmastermind.com

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@
 #https://gist.github.com/arif98741/6a3881ac7ca93d3e86c37bf676e3c016
 echo \
 
-if cd /var/www/html/mastermindabacusbd.com; then
+if cd /var/www/html/playschoolmastermind.com; then
 
 	echo "=========================================================="
 	echo "Welcome to Shell for Pulling"
@@ -19,14 +19,14 @@ if cd /var/www/html/mastermindabacusbd.com; then
 
  	sudo git branch
 	#sudo git fetch --all
-	sudo git checkout production_abacus
+	sudo git checkout production_school
 	sudo git  pull
 	echo \
-	echo "Getting data from branch: production_abacus"
+	echo "Getting data from branch: production_school"
 
 	echo \
 	echo "Thanks for using shell command"
 
 else
-	echo "could not run shell on var/www/html/mastermindabacusbd.com"
+	echo "could not run shell on var/www/html/playschoolmastermind.com"
 fi
